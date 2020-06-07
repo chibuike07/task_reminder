5fe034e48296d25d88f858abc23aaafbd96655b0
hobbyluv <princewillchime43@gmail.com>
2020-06-07 11:34:41 +0100

    adjusted a file name

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 // import RoutesPages from "./Components/router";
-import Form from "./practiceTodo/Todo";
+import Form from "./Components/Todo";
 import * as serviceWorker from "./serviceWorker";
 
 // let todoItems = [
